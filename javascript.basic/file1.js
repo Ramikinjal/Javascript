98768abe3849b9042ca895927b3c5dc08e0d9568
Ramikinjal <kinjal.rami2002@gmail.com>
2023-09-10 22:22:50 +0530

    JS

---
+++ javascript.basic/file1.js
@@ -0,0 +1,4 @@
+// console.log can print somrthing on console
+console.log("hello kinjal");
+console.log('hello kinjal rami');
+console.log(`hello kinjal khodakiya`);
---
+++ javascript.basic/variable.file2.js
@@ -0,0 +1,63 @@
+
+
+
+"use strict";
+
+
+// Intro in VAriables
+
+// variables can store some information
+// we can use that information later
+// we can change that information later
+
+// declare a variable
+
+var firstName = "kinjal";
+
+
+// use a variable
+console.log(firstName);
+// console.log(firstname);->but give to error(firstname is not defind)
+
+// change value
+firstName = "rami";
+console.log(firstName);
+//  ek value ko change kiya.
+
+
+
+// [second view]
+var firstName = "kinjal";
+console.log(firstName);
+
+
+var firstName = "rami";
+console.log(firstName);
+// value change ki but perticular value ko alg-alg variable lagak.
+// means dono(2) variable ki  particular alg-alg ek value.
+// or kuchh nai 2 bar variable declare hua.
+
+
+// [third view](vraiable ke bina)
+firstName = "kinjal";
+console.log(firstName);
+
+
+
+
+ firstname = "rami";
+console.log(firstName);
+// console.log(firstname);
+
+
+// console.log(firstName);
+// ye bina variable ke chalta he but ye age jake dhhikkat krta he.
+// ab hame aisa krna he ki variable ke bina chalna nahi chahei to .
+// sabse uper "use strict" lagao to javascript hame galti krne se bachata he means k error deta he. 
+
+
+
+
+
+
+
